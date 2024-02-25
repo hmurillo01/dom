026c0f2ed04c31c15b055048872c870e342666a4
David Pulido <davidpulido1232019@gmail.com>
2024-02-24 19:40:49 -0500

    Ejemplo formulario instru

---
+++ main.js
@@ -0,0 +1,15 @@
+const ficha = document.getElementById("ficha");
+
+ficha.addEventListener('blur', () => {
+  // alert(ficha.value)
+  if (ficha.value == "") {
+    alert("llene al campo ficha")
+  } else {
+    if (ficha.value == isInteger) {
+
+    }
+
+  }
+});
+
+
